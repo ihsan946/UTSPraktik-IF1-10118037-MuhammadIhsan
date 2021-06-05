bf5106b76e567fc926f8c44b4419337562943578
Ihsan946 <ihsanmuhammad946@gmail.com>
2021-06-05 11:09:00 +0700

    Initial commit

---
+++ app/src/main/java/com/app/ihsan946/utspraktik_if1_10118037_muhammadihsan/MainActivity.java
@@ -0,0 +1,14 @@
+package com.app.ihsan946.utspraktik_if1_10118037_muhammadihsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
